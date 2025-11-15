41b32ce98d3fd3c53f8aed28a547c67249d50f33
mrsong <huynhnhanquoc@gmail.com>
2025-11-15 14:32:23 +0700

    update v

--- dist/kityaml.js
+++ dist/kityaml.js
@@ -2,105 +2,76 @@
 	const kitmodule = global.kitmodule || (global.kitmodule = {});
 	const $ = Symbol("$");
 
-	function KitFrontMatter(header = {}, body = "") {
+	function KitYAML(obj = {}) {
+		if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+			throw new Error("KitYAML: input must be a plain object.")
+		}
 		this[$] = {
-			header,
-			body,
-			inline: !0,
+			obj,
+			inline: !0
 		}
 	}
-	KitFrontMatter.prototype.isBlock = function(val = !0) {
-		this[$].inline = !val;
+	KitYAML.prototype.inline = function(enable = !0) {
+		this[$].inline = enable;
 		return this
 	};
-	KitFrontMatter.prototype.isInline = function(val = !0) {
-		this[$].inline = val;
+	KitYAML.prototype.block = function(enable = !0) {
+		this[$].inline = !enable;
 		return this
+	};
+	KitYAML.prototype.convert = function() {
+		const {
+			obj,
+			inline
+		} = this[$];
+		return encodeYAML(obj, 0, inline)
+	};
+	KitYAML.prototype.frontMatter = function(body = "") {
+		return `---\n${this.convert()}\n---\n\n${body}`
 	};
 
-	function safeYAML(value, indent = 0, inline = !1) {
+	function encodeYAML(value, indent = 0, inline = !1) {
 		const space = "  ".repeat(indent);
 		if (value === null || value === undefined) return '""';
 		if (typeof value === "boolean" || typeof value === "number") return value;
 		if (typeof value === "string") return JSON.stringify(value);
 		if (Array.isArray(value)) {
 			if (inline) {
-				return `[${value.map(v => safeYAML(v, indent, inline)).join(", ")}]`
+				return `[${value.map(v => encodeYAML(v, indent, inline)).join(", ")}]`
 			} else {
 				return value.map(v => {
 					if (typeof v === "object" && v !== null) {
-						return `\n${space}- ${safeYAML(v, indent + 1).replace(/^\s+/, "")}`
+						return `\n${space}- ${encodeYAML(v, indent + 1, inline).replace(/^\s+/, "")}`
 					} else {
-						return `${space}- ${safeYAML(v, indent + 1)}`
+						return `${space}- ${encodeYAML(v, indent + 1, inline)}`
 					}
 				}).join("\n")
 			}
 		}
 		if (typeof value === "object") {
-			return Object.entries(value).map(([k, v]) => {
-				if (Array.isArray(v)) {
+			return Object.entries(value).map(([key, val]) => {
+				if (Array.isArray(val)) {
 					if (inline) {
-						return `${space}${k}: [${v.map(item => safeYAML(item, indent, inline)).join(", ")}]`
+						return `${space}${key}: [${val.map(i => encodeYAML(i, indent, inline)).join(", ")}]`
 					} else {
-						return `${space}${k}:\n${safeYAML(v, indent + 1, inline)}`
+						return `${space}${key}:\n${encodeYAML(val, indent + 1, inline)}`
 					}
-				} else if (typeof v === "object" && v !== null) {
-					return `${space}${k}:\n${safeYAML(v, indent + 1, inline)}`
-				} else {
-					return `${space}${k}: ${safeYAML(v, indent + 1, inline)}`
 				}
+				if (typeof val === "object" && val !== null) {
+					return `${space}${key}:\n${encodeYAML(val, indent + 1, inline)}`
+				}
+				return `${space}${key}: ${encodeYAML(val, indent + 1, inline)}`
 			}).join("\n")
 		}
 		return JSON.stringify(value)
 	}
-	KitFrontMatter.prototype.toYAML = function() {
-		const {
-			header,
-			inline
-		} = this[$];
-		return safeYAML(header, 0, inline)
-	};
-	KitFrontMatter.prototype.toMarkdown = function() {
-		return `---\n${this.toYAML()}\n---\n\n${this[$].body}`
-	};
-
-	function headerToYAML(header = {}, options = {}) {
-		const fm = new KitFrontMatter(header);
-		if (options.blockArrays) fm.isBlock(!0);
-		if (typeof options.indent === "number") {
-			fm[$].customIndent = options.indent
-		}
-		return fm.toYAML()
-	}
-
-	function objectToYAML(obj = {}, options = {}) {
-		const inline = !(options.blockArrays === !0);
-		const indent = options.indent || 0;
-		return safeYAML(obj, indent, inline)
-	}
-
-	function frontMatterMarkdown(header = {}, body = "", options = {}) {
-		const fm = new KitFrontMatter(header, body);
-		if (options.blockArrays) fm.isBlock(!0);
-		return fm.toMarkdown()
-	}
-	global.KitFrontMatter = KitFrontMatter;
-	global.headerToYAML = headerToYAML;
-	global.objectToYAML = objectToYAML;
-	global.frontMatterMarkdown = frontMatterMarkdown;
-	kitmodule.FrontMatter = KitFrontMatter;
-	kitmodule.frontMatter = function(header, body) {
-		return KitFrontMatter(header, body)
-	};
-	kitmodule.headerToYAML = headerToYAML;
-	kitmodule.objectToYAML = objectToYAML;
-	kitmodule.frontMatterMarkdown = frontMatterMarkdown;
+	global.KitYAML = KitYAML;
+	global.yamlFrontMatter = (obj, body) => new KitYAML(obj).frontMatter(body);
+	kitmodule.YAML = KitYAML;
+	kitmodule.yaml = (obj) => new KitYAML(obj);
 	if (typeof module !== "undefined" && module.exports) {
 		module.exports = {
-			KitFrontMatter,
-			headerToYAML,
-			objectToYAML,
-			frontMatterMarkdown
+			KitYAML
 		}
 	}
 })(typeof window !== "undefined" ? window : globalThis)
